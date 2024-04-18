d32b5763b8f578060356fd77c1fcecbe602ee110
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-04-18 09:07:01 -0300

    Latex + no autoscroll

--- src/components/documents/components/tiptap/index.tsx
+++ src/components/documents/components/tiptap/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 import css from "./tiptap.module.css"
+import 'katex/dist/katex.min.css'
 
 import { useEditor, EditorContent } from '@tiptap/react'
 
@@ -7,6 +8,7 @@ import StarterKit from '@tiptap/starter-kit'
 import TaskList from '@tiptap/extension-task-list'
 import TaskItem from '@tiptap/extension-task-item'
 import Highlight from '@tiptap/extension-highlight'
+import MathExtension from '@aarkue/tiptap-math-extension'
 
 import BubbleMenu from "./components/bubbleMenu"
 import { DocumentState } from "../../../../types/document"
@@ -29,16 +31,12 @@ const Tiptap = ({document, ...props}: TipTapProps) => {
       }),
       Highlight.configure({
         HTMLAttributes: { class: css["highlight"] }
-      })
+      }),
+      MathExtension.configure({ evaluation: true, addInlineMath:true })
     ],
     content: document.content,
     onUpdate: ({ editor }) => {
       props.onUpdate(document, editor.getHTML())
-      const mainH = window.document.querySelector("main")?.clientHeight
-      const screenH = window.innerHeight
-      if (mainH && screenH) {
-        window.scroll({top:(mainH + 32) - screenH}) 
-      }
     }
   })
 
