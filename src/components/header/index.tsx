1d128316ab6b3954f9cee2a57d25348c8d53cd86
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-04-29 09:23:49 -0300

    change "config" from "menu"

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,13 @@
 import css from "./header.module.css"
 
 import Tabs from "./components/tabs";
-import Config from "./components/config";
+import Menu from "./components/menu";
 
 export default function Header () {
   return (
     <header id={css["header"]}>
       <Tabs />
-      <Config />
+      <Menu />
     </header>
   ) 
 }
