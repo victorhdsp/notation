d56c01792cbcbe70669a5a240330f7a29a0a642c
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-04-29 09:21:19 -0300

    remove autoselect tab

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,29 +3,14 @@ import css from './app.module.css'
 import Header from "../components/header";
 import Documents from "../components/documents";
 import Actions from "../components/actions";
-import useDocumentStore from "../store/documentStore";
-import useGlobalStore from "../store/globalStore";
 import useConfigStore from "../store/configStore";
 
 import { useEffect } from "react";
 import { appWindow } from "@tauri-apps/api/window"
 
 function App() {
-  const firstDocument = useDocumentStore((store) => store.documents[0])
-  const documents = useDocumentStore((store) => store.documents)
-  const {selectDocument, selectedDocumentId, addDocumentToTab} = useGlobalStore()
   const opacityBackground = useConfigStore((state) => state.opacityBackground)
-  
-  const document = documents.find(doc => doc.id === selectedDocumentId)
-
-  if (!selectedDocumentId || !document && firstDocument) {
-    selectDocument(firstDocument.id)
-    addDocumentToTab(firstDocument.id)
-  }
-
-  useEffect(() => {
-    appWindow.setDecorations(true)
-  }, [])
+  useEffect(() => { appWindow.setDecorations(true) }, [])
 
   return (
     <div 
